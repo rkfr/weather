766f7dfa3326e4a778693329c6c817f75039adf8
rkfr <e.xbyvnqn@gmail.com>
2020-02-12 18:14:07 +0200

    removed exports, added formatWeatherResponse function

--- src/helpers.js
+++ src/helpers.js
@@ -2,9 +2,9 @@ const getHours = (hours) => (Number(hours) < 10 ? `0${hours}` : String(hours));
 
 const to24 = (period, hours) => (period === 'PM' ? Number(hours) + 12 : hours);
 
-export const kelvinToCelsius = (k) => (k ? Math.ceil(k - 273.15) : 0);
+const kelvinToCelsius = (k) => (k ? Math.ceil(k - 273.15) : 0);
 
-export const getTomeFromMilliseconds = (ms) => {
+const getTimeFromUnixMs = (ms) => {
   const date = new Date(ms * 1000);
   const time = date.toLocaleTimeString().split(':');
   const [hours, minutes, secondsWithPeriod] = time;
@@ -12,4 +12,32 @@ export const getTomeFromMilliseconds = (ms) => {
 
   const convertedhours = getHours(to24(period, hours));
   return `${convertedhours}:${minutes}`;
+};
+
+export const formatWeatherResponse = (weatherData) => {
+  const {
+    // eslint-disable-next-line camelcase
+    main: { temp, temp_min, temp_max },
+    weather: weatherArray,
+    wind,
+    sys,
+    name: locationName,
+  } = weatherData;
+
+  const { main: weatherState, description, icon } = weatherArray[0];
+  const { country, sunrise: sunriseMs, sunset: sunsetMs } = sys;
+
+  return {
+    locationName,
+    country,
+    weatherState,
+    description,
+    sunrise: getTimeFromUnixMs(sunriseMs),
+    sunset: getTimeFromUnixMs(sunsetMs),
+    temperature: kelvinToCelsius(temp),
+    tempMin: kelvinToCelsius(temp_min),
+    tempMax: kelvinToCelsius(temp_max),
+    windSpeed: wind.speed || 0,
+    icon,
+  };
 };

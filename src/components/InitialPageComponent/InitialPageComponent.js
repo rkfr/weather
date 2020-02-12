f12da7324ccb69dd9b65c9da37eca5751e2e288d
rkfr <e.xbyvnqn@gmail.com>
2020-02-12 18:20:06 +0200

    completely rewritten

--- src/components/InitialPageComponent/InitialPageComponent.js
+++ src/components/InitialPageComponent/InitialPageComponent.js
@@ -1,73 +1,93 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './InitialPageComponent.scss';
 
 import { Input } from '../Input';
 import { Loader } from '../Loader';
-// import {} from '../../api';
 
 const InitialPageComponent = (props) => {
   const {
+    loadWeatherByGeolocation,
+    locationQuery,
+    changeLocationQuery,
+    loadWeather,
     weatherData,
     isLoading,
     error,
-    loadWeather,
   } = props;
 
+  useEffect(() => {
+    loadWeatherByGeolocation();
+  }, []);
 
-  const load = () => {
-    loadWeather('odessa');
+  const onLocationQueryChange = ({ target }) => {
+    changeLocationQuery(target.value);
+  };
+
+  const onSubmitWeather = () => {
+    loadWeather(locationQuery);
   };
 
   return (
 
-    <div>
-      <div>{isLoading ? 'loading' : 'false'}</div>
-      <button type="button" onClick={load}>load</button>
+    <div className="initial-page">
+      {isLoading && (
+        <Loader />
+      )}
+
+      <div className="content">
+        <div className="initial-page__input-wrapper">
+
+          <Input
+            value={locationQuery}
+            onChange={onLocationQueryChange}
+            onSubmit={onSubmitWeather}
+            errorMessage={error}
+          />
+          {(weatherData && !error) && (
+          <div className="initial-page__location">
+            <span>Show weather forecast for </span>
+            <Link className="initial-page__link" to={`/weather/${weatherData.locationName}`}>{weatherData.locationName}</Link>
+          </div>
+          )}
+
+        </div>
+      </div>
     </div>
-
-  // <div className="initial-page">
-  //   {isLoading && (
-  //     <Loader />
-  //   )}
-
-  //   <div className="content">
-  //     <div className="initial-page__input-wrapper">
-
-  //       <Input
-  //         isError={!!errorMessage}
-  //         errorMessage={errorMessage}
-  //         value={cityQuery}
-  //         onChange={setCity}
-  //         onSubmit={loadWeather}
-  //       />
-  //       {(weather && !errorMessage && cityQuery) && (
-  //       <div className="initial-page__location">
-  //         <span>Show weather forecast for </span>
-  //         <Link className="initial-page__link" to={`/weather/${weather.name}`}>{weather.name}</Link>
-  //       </div>
-  //       )}
-
-  //     </div>
-  //   </div>
-
-
-  // </div>
   );
 };
 
 InitialPageComponent.propTypes = {
-  // eslint-disable-next-line react/forbid-prop-types
-  weatherData: PropTypes.object,
+  locationQuery: PropTypes.string,
+  weatherData: PropTypes.shape({
+    locationName: PropTypes.string,
+    country: PropTypes.string,
+    weatherState: PropTypes.string,
+    description: PropTypes.string,
+    sunrise: PropTypes.string,
+    sunset: PropTypes.string,
+    temperature: PropTypes.number,
+    tempMin: PropTypes.number,
+    tempMax: PropTypes.number,
+    windSpeed: PropTypes.number,
+    icon: PropTypes.string,
+  }),
   error: PropTypes.string,
   isLoading: PropTypes.bool,
+  loadWeather: PropTypes.func,
+  changeLocationQuery: PropTypes.func,
+  loadWeatherByGeolocation: PropTypes.func,
 };
 
 InitialPageComponent.defaultProps = {
+  locationQuery: [],
   weatherData: [],
   error: null,
   isLoading: false,
+  loadWeather: () => {},
+  changeLocationQuery: () => {},
+  loadWeatherByGeolocation: () => {},
 };
 
 export default InitialPageComponent;
--- src/components/InitialPageComponent/index.js
+++ src/components/InitialPageComponent/index.js
@@ -1,11 +1,22 @@
 
 import { connect } from 'react-redux';
-import { getCurrentWeather, getLoading, getError } from '../../store/selectors';
-
 import InitialPageComponent from './InitialPageComponent';
-import { loadCurrentWeather } from '../../store/actions';
+
+import {
+  getLocationQuery,
+  getCurrentWeather,
+  getLoading,
+  getError,
+} from '../../store/selectors';
+
+import {
+  loadCurrentWeather,
+  setLocationQuery,
+  loadCurrentWeatherByGeolocation,
+} from '../../store/actions';
 
 const mapStateToProps = (state) => ({
+  locationQuery: getLocationQuery(state),
   weatherData: getCurrentWeather(state),
   isLoading: getLoading(state),
   error: getError(state),
@@ -13,6 +24,8 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   loadWeather: (weather) => dispatch(loadCurrentWeather(weather)),
+  changeLocationQuery: (query) => dispatch(setLocationQuery(query)),
+  loadWeatherByGeolocation: () => dispatch(loadCurrentWeatherByGeolocation()),
 });
 
 export default connect(

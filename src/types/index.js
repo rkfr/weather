4dc707450b637c23668e5e59854c613057fe613e
rkfr <e.xbyvnqn@gmail.com>
2020-02-14 16:24:18 +0200

    types has been separated

--- src/types/index.js
+++ src/types/index.js
@@ -1,34 +1,26 @@
 import PropTypes from 'prop-types';
 
-export const currentWeatherType = {
-  locationQuery: PropTypes.string,
-  weatherData: PropTypes.shape({
-    locationName: PropTypes.string,
-    country: PropTypes.string,
-    weatherState: PropTypes.string,
-    description: PropTypes.string,
-    sunrise: PropTypes.string,
-    sunset: PropTypes.string,
-    temperature: PropTypes.number,
-    tempMin: PropTypes.number,
-    tempMax: PropTypes.number,
-    windSpeed: PropTypes.number,
-    humidity: PropTypes.number,
-    icon: PropTypes.string,
-  }),
-  error: PropTypes.string,
-  isLoading: PropTypes.bool,
-  loadWeather: PropTypes.func,
-  changeLocationQuery: PropTypes.func,
-  loadWeatherByGeolocation: PropTypes.func,
-};
+export const forecastWeatherType = PropTypes.shape({
+  dt: PropTypes.number,
+  time: PropTypes.string,
+  icon: PropTypes.string,
+  temperature: PropTypes.number,
+  description: PropTypes.string,
+});
 
-export const currentWeatherTypeDefault = {
-  locationQuery: [],
-  weatherData: [],
-  error: null,
-  isLoading: false,
-  loadWeather: () => {},
-  changeLocationQuery: () => {},
-  loadWeatherByGeolocation: () => {},
-};
+export const forecastType = PropTypes.arrayOf(forecastWeatherType);
+
+export const currentWeatherType = PropTypes.shape({
+  locationName: PropTypes.string,
+  country: PropTypes.string,
+  weatherState: PropTypes.string,
+  description: PropTypes.string,
+  sunrise: PropTypes.string,
+  sunset: PropTypes.string,
+  temperature: PropTypes.number,
+  tempMin: PropTypes.number,
+  tempMax: PropTypes.number,
+  windSpeed: PropTypes.number,
+  humidity: PropTypes.number,
+  icon: PropTypes.string,
+});

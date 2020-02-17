17924c9bf0a43b69f39fb79078fdc02ed10279c0
rkfr <e.xbyvnqn@gmail.com>
2020-02-17 16:32:49 +0200

    added route for error

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import { Switch, Route } from 'react-router-dom';
 import InitialPageComponent from './components/InitialPageComponent';
 import Weather from './components/Weather';
 import Forecast from './components/Forecast';
+import ErrorPage from './components/ErrorPage';
 
 const App = () => (
   <div className="app">
     <Switch>
+      <Route exact path="/">
+        <InitialPageComponent />
+      </Route>
       <Route path="/weather/:id">
         <Weather />
       </Route>
       <Route path="/forecast/:id">
         <Forecast />
       </Route>
-      <Route path="/">
-        <InitialPageComponent />
+      <Route>
+        <ErrorPage />
       </Route>
     </Switch>
   </div>
